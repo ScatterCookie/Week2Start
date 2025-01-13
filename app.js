c7ed872776ba6021cb979e7154dd9b88ccaf662c
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-13 10:59:13 +0000

    simple number counter with plus and minus

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 let counterDisplayElem = document.querySelector(".counter-display");
+let counterMinusElem = document.querySelector(".counter-minus");
 let counterPlusElem = document.querySelector(".counter-plus");
 
 let count = 0;
 
 updateDisplay();
 
-//after click execute function
-counterPlusElem.addEventListener("click", () => {});
-//adds 1 to the number
 counterPlusElem.addEventListener("click", () => {
-  counter++;
-  counterDisplayElem.innerHTML = count;
+  count++;
+  updateDisplay();
+});
+
+counterMinusElem.addEventListener("click", () => {
+  count--;
+  updateDisplay();
 });
 
 function updateDisplay() {
   counterDisplayElem.innerHTML = count;
-  updateDisplay();
 }
